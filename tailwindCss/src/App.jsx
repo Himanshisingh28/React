bf972480fad3c5dfda708c30c90506abf41c880b
Himanshisingh28 <himanshikashyap200528@gmail.com>
2026-06-29 14:04:22 +0530

    learn about props

--- tailwindCss/src/App.jsx
+++ tailwindCss/src/App.jsx
@@ -7,12 +7,17 @@ import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
+  const myObj={
+    name: "himanshi",
+    age:20,
+    password:123
+  }
 
   return (
     <>
       <h1 className="bg-green-400 text-white p-4 rounded-xl p-7">Tailwind css</h1>
-      <Card/>
-      <Card/>
+      <Card username="himanshi" btntext="view"/>
+      <Card username="monkey" btntext="hehe"/>
 
     </>
   )
--- tailwindCss/src/components/card.jsx
+++ tailwindCss/src/components/card.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-function Card() {
+function Card({username, btntext}) {
+      console.log( username);
+      
       return (
-            <div class="flex flex-col items-center gap-6 p-7 md:fl rounded-2xl">
+            <div className="flex flex-col items-center gap-6 p-7 md:fl rounded-2xl">
                   <div>
-                        <img class="size-48 shadow-xl rounded-md" alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXT3ziMz6XDysCIrSSrs-vDTwAbNWd8iMnHv5kJ7bSvg&s&ec=121801496" />
+                        <img className="size-48 shadow-xl rounded-md" alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXT3ziMz6XDysCIrSSrs-vDTwAbNWd8iMnHv5kJ7bSvg&s&ec=121801496" />
                   </div>
-                  <div class="flex items-center">
-                        <span class="text-2xl font-medium">Class Warfare</span>
-                        <span class="font-medium text-sky-500">The Anti-Patterns</span>
-                        <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+                  <div className="flex items-center">
+                        <span className="text-2xl font-medium">Class Warfare</span>
+                        <span className="font-medium text-sky-500">{username}</span>
+                        <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
                               <span>No. 4</span>
                               <span>·</span>
                               <span>2025</span>
                         </span>
+                        <button className="bg-green-400 text-white rounded-xl p-3">{btntext}</button>
                   </div>
             </div>
       )
