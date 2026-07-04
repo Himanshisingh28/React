d2516cc4a8c81dd96ff62373b10e34c3cd545a5a
Himanshisingh28 <himanshikashyap200528@gmail.com>
2026-07-04 21:50:04 +0530

    add new project-currencyChanger

---
+++ currency-convertor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className=' text-3xl bg-blue-200'>I am currency-convertor</h1>
+    </>
+  )
+}
+
+export default App
