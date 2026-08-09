755d5fe533350d7f11bd3aa7f09aeb5718eaaf99
Himanshisingh28 <himanshikashyap200528@gmail.com>
2026-08-09 16:01:01 +0530

    add new component

---
+++ MegaProject/src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import {Link, useNavigate} from 'react-router-dom'
+
+function Login (){
+      return(
+      <div>to</div>
+      )
+}
