a7f4d9ad1bd39389469b2b50382d87fdbe55a317
Himanshisingh28 <himanshikashyap200528@gmail.com>
2026-08-16 12:35:12 +0530

    add create form

--- MegaProject/src/components/index.js
+++ MegaProject/src/components/index.js
@@ -3,11 +3,12 @@ import Footer from "./footer/footer";
 import Container from "./container/Container";
 import Logo from "./logo";
 import LogOutbtn from "./Header/LogOutbtn";
-
+import RTE from "./RTE";
 export {
       Header,
       footer,
       Container,
       Logo,
-      LogOutbtn
+      LogOutbtn,
+      RTE
 }
---
+++ MegaProject/src/components/post-form/PostForm.jsx
@@ -0,0 +1,80 @@
+import React, { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import { Button, Input, Select, RTE } from '../index'
+import appwriteServide from "../../appwrite/config"
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+function PostForm({ post }){
+      const { register, handleSubmit, watch, setValue, control, getValues } = useForm({
+            defaultValues: {
+                  title: post?.title || '',
+                  slug: post?.slug || '',
+                  content: post?.content || '',
+                  status: post?.status || 'active',
+            }
+      })
+      const navigate = useNavigate()
+      const userData = useSelector(state => state.user.userData)
+
+      const submit = async (data) => {
+            if (post) {
+                  const file = data.image[0] ? appwriteServide.uploadFile(data.image[0]) : null
+                  if (file) {
+                        appwriteServide.deleteFile(post.featuredImage)
+                  }
+                  const dbPost = await appwriteServide.updatePost
+                        (post.$id, {
+                              ...data,
+                              featuredImage: file ? file.$id : undefined,
+                        })
+                  if (dbpost) {
+                        navigate(`/post/${dbpost.$id}`)
+                  }
+            }else{
+                  const file = await appwriteServide.uploadFile
+                  (data.image[0]);
+
+                  if (file) {
+                        const fileId = file.$id
+                        data.featuredImage = fileId
+                        await appwriteServide.createPost({
+                              ...data,
+                              userId: userData.$id,
+                        })
+                        if(dbPost){
+                              navigate(`/post/${dbPost.$id}`)
+                        }
+                  }
+            }
+
+      }
+      }
+      const slugTransform = useCallback((value)=>{
+            if (value && typeof value === 'string') 
+            return value
+                  .trim()
+                  .toLowerCase()
+                  .replace(/^[a-zA-Z\d\s]+/g, '-')
+                  .replace(/\s/g, '-')
+            return ''
+      },[])
+      React.useEffect(() => {
+            const subscription = watch((value, {name})=>{
+                  if (name === 'title') {
+                        setValue('slug', slugTransform(value.title,{shouldValidate: true}))     
+                  }
+            })
+
+            return () => {
+                  subscription.usSubscribe()
+            }
+      },[watch, slugTransform, setValue])
+return (
+      <div>
+
+      </div>
+)
+
+
+export default PostForm
