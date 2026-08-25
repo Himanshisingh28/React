fc7c8418a806e489dbfcd130d5440d406190e97e
Himanshisingh28 <himanshikashyap200528@gmail.com>
2026-08-25 18:03:59 +0530

    fix a bug in pages

--- MegaProject/src/pages/Home.jsx
+++ MegaProject/src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home () {
       const [posts, setPosts] = useState([])
 
       useEffect(() => {
-            appwriteservice.getPost().then((posts) =>{
+            appwriteservice.getPosts().then((posts) =>{
                   if(posts){
                         setPosts(posts.documents)
                   }
