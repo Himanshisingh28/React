dd4159037321ea0331848d0a2118bab481fff489
Himanshisingh28 <himanshikashyap200528@gmail.com>
2026-08-27 17:05:58 +0530

    Fix PostForm bugs: typos, wrong Redux path, missing await

--- MegaProject/src/components/RTE.jsx
+++ MegaProject/src/components/RTE.jsx
@@ -1,33 +1,24 @@
 import React from 'react'
-import { Editor } from '@tinymce/tinymce-react'
 import { Controller } from 'react-hook-form'
 
-export default function RTE({ name, control, label, defaultValue = " " }) {
-  return (
-    <div className='w-full'>
-      {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
-      <Controller
-        name={name || "content"}
-        control={control}
-        render={({ field: { onChange } }) => (
-          <Editor
-            initialValue={defaultValue}
-            init={{
-              initialValue: defaultValue,
-              height: 500,
-              menubar: true,
-              plugins: [
-                'advlist autolink lists link image charmap print preview anchor help',
-                'searchreplace visualblocks code fullscreen'],
-              toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-              content_style: "body { font-family:Helvetica,"
-            }}
-            onEditorChange={onchange}
+export default function RTE({ name, control, label, defaultValue = "" }) {
+    return (
+        <div className="w-full">
+            {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+            <Controller
+                name={name || "content"}
+                control={control}
+                defaultValue={defaultValue}
+                render={({ field: { onChange, value } }) => (
+                    <textarea
+                        value={value}
+                        onChange={onChange}
+                        rows={10}
+                        className="w-full px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 border border-black/10"
+                        placeholder="Write your content here..."
+                    />
+                )}
             />
-        )}
-      />
-    </div>
-  )
+        </div>
+    )
 }
-
-
